8ca1eda443449023034a29f73d1b9193b2c13dd7
vanbong <microwave88@gmail.com>
2023-06-17 23:34:30 +0700

    Before using sqlalchemy

--- app/main.py
+++ app/main.py
@@ -31,20 +31,45 @@ while True:
 
 @app.get("/")
 def read_root():
-    return {"Hello from root"}
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    return {f"Here is post with id: {id}"}
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", str(id))
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+    return {"post_detail": post}
 
-@app.post("/posts")
-def created_posts(post: Post):
-    return {"Hello, this is post function"}
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
-@app.delete("/posts/{id}")
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    return {f"Deleted post with id: {id}"}
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", str(id))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exits")
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int):
-    return {f"Updated post with id: {id}"}
+def update_post(id: int, post: Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exits")
+    return {"data": updated_post}
